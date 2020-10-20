27042ee1f0e06b4e842f8a8923b292d7bfdf6932
Eli Vosniak <eli.vosniak@gmail.com>
2020-10-20 15:39:34 -0400

    Moved SidebarFragment to correct package

--- app/src/main/java/com/websmobileapps/musicdeck/Fragments/SidebarFragment.java
+++ app/src/main/java/com/websmobileapps/musicdeck/Fragments/SidebarFragment.java
@@ -1,10 +1,12 @@
-package com.websmobileapps.musicdeck;
+package com.websmobileapps.musicdeck.Fragments;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.websmobileapps.musicdeck.R;
 
 public class SidebarFragment extends Fragment {
     @Override
