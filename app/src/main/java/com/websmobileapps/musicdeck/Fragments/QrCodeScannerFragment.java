35a8ba6f1cebe7c5cf55fdc30165d44cf1215271
FinisAeternum <32653454+FinisAeternum@users.noreply.github.com>
2020-11-04 11:52:40 -0500

    Qrcode (#4)
    * Added a way to display QR codes. don't know if it works perfectly
    
    * Added a qr code scanner fragment, might work, don't know
    
    * Added camera permission to manifest file

---
+++ app/src/main/java/com/websmobileapps/musicdeck/Fragments/QrCodeScannerFragment.java
@@ -0,0 +1,50 @@
+package com.websmobileapps.musicdeck.Fragments;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.datepicker.MaterialTextInputPicker;
+import com.google.zxing.Result;
+import com.websmobileapps.musicdeck.MainActivity;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class QrCodeScannerFragment extends Fragment implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        mScannerView = new ZXingScannerView(getActivity());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return mScannerView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        Toast.makeText(getContext(), rawResult.getText(), Toast.LENGTH_SHORT).show();
+    }
+}
