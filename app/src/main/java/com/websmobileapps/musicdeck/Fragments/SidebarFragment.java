443014246c64c0c1de411e159fe1ec1a7dcc634d
Webs <danweber9000@gmail.com>
2020-11-25 15:00:27 -0500

    Added profile view. Updated nav graph to final version, with placeholder for deck edit. Updated signup and login fragments to remove old code and correct navigations. Updated sidebar with home and delete account buttons. Wired up sidebar. Sidebar text view now updates.

--- app/src/main/java/com/websmobileapps/musicdeck/Fragments/SidebarFragment.java
+++ app/src/main/java/com/websmobileapps/musicdeck/Fragments/SidebarFragment.java
@@ -2,6 +2,7 @@ package com.websmobileapps.musicdeck.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -11,14 +12,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+import com.websmobileapps.musicdeck.Model.DatabaseUser;
 import com.websmobileapps.musicdeck.R;
+import com.websmobileapps.musicdeck.Repository.Repo;
 import com.websmobileapps.musicdeck.ViewModels.AuthViewModel;
+
+import java.util.Objects;
 
 public class SidebarFragment extends Fragment {
 
     private View mSidebarFragment;
     private Button mQRCodeBTN, mProfileBTN, mSignOutBTN, mDelAccBTN, mHomeBTN;
+    private TextView mCurrentUserTV;
 
     private AuthViewModel mAuthViewModel;
 
@@ -33,6 +43,22 @@ public class SidebarFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mSidebarFragment = inflater.inflate(R.layout.sidebar_view, container, false);
+
+        mCurrentUserTV = mSidebarFragment.findViewById(R.id.SB_current_user);
+        final String userUID = Objects.requireNonNull(mAuthViewModel.getUserMutableLiveData().getValue()).getUid();
+        Repo.getInstance().getUserRef(userUID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                DatabaseUser user = snapshot.getValue(DatabaseUser.class);
+                String username = Objects.requireNonNull(user).getUsername();
+                mCurrentUserTV.setText(username);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         mHomeBTN = mSidebarFragment.findViewById(R.id.home_button);
         mHomeBTN.setOnClickListener(new View.OnClickListener() {
