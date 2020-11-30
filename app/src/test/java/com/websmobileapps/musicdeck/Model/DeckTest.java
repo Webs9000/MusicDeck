95756b0ee058feed3018d3b08892dc45b1fa0283
Eli Vosniak <eli.vosniak@gmail.com>
2020-11-29 21:14:29 -0500

    Added getter and setter tests for deck test

--- app/src/test/java/com/websmobileapps/musicdeck/Model/DeckTest.java
+++ app/src/test/java/com/websmobileapps/musicdeck/Model/DeckTest.java
@@ -3,22 +3,32 @@ package com.websmobileapps.musicdeck.Model;
 import junit.framework.TestCase;
 
 public class DeckTest extends TestCase {
+    Deck testDeck = new Deck("testCreator", "testTitle", "testSubject");
 
     public void testGetTitle() {
+        assertEquals("testTitle", testDeck.getTitle());
     }
 
     public void testSetTitle() {
+        testDeck.setTitle("testTitle1");
+        assertEquals("testTitle1", testDeck.getTitle());
     }
 
     public void testGetCreator() {
+        assertEquals("testCreator", testDeck.getCreator());
     }
 
     public void testSetCreator() {
+        testDeck.setCreator("testCreator1");
+        assertEquals("testCreator1", testDeck.getCreator());
     }
 
     public void testGetSubject() {
+        assertEquals("testSubject", testDeck.getSubject());
     }
 
     public void testSetSubject() {
+        testDeck.setSubject("testSubject1");
+        assertEquals("testSubject1", testDeck.getSubject());
     }
 }
